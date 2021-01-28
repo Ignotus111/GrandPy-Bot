0f1c1b6a5747b8cccadf9f7a3032af130535190f
Ignotus111 <romainlevasseur4@gmail.com>
2021-01-28 16:51:36 +0100

    app structure, python function definition

---
+++ app/func.py
@@ -0,0 +1,22 @@
+"""
+recevoir et analyser le contenu du formulaire
+    -recoit contenu form, le coupe mot par mot
+    -recupère chaque mot, ne garde que les mots-clés
+    -
+
+interaction avec l'api google map
+    -envoyer une requete
+    -recuperer reponse
+    -
+
+interaction media wiki
+    -envoyer requete
+    -recuperer reponse
+    -
+
+envoyer les informations récupérée à l'utilisateur
+    -donner reponse google map
+    -afficher google map
+    -donner reponse media wiki
+
+"""
