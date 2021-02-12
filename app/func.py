26106836d590b6ef6c1cae9f9366f3f3b156e2d1
Ignotus111 <romainlevasseur4@gmail.com>
2021-02-12 03:28:28 +0100

    tests, template

--- app/func.py
+++ app/func.py
@@ -1,7 +1,32 @@
+from constants import stopWords
+
+
+def formulaire(contenuDuForm):
+    if type(contenuDuForm) == list:
+        print("C'est bon!")
+    else:
+        print("Pas bon! Ce n'est pas une liste!")
+
+def motsCles(listDeMotsCles):
+    if type(listDeMotsCles) == list:
+        if not listDeMotsCles:
+            print("Pas bon! La liste est vide")
+        else:
+            if any(elem in stopWords for elem in listDeMotsCles):
+                print("Pas bon! Un ou plusieurs mots ne doivent pas être dans cette liste!")
+            else:
+                print("OK!")
+    else:
+        print("Pas bon! Ce n'est pas une liste!")
 """
 recevoir et analyser le contenu du formulaire
     -recoit contenu form, le coupe mot par mot
     -recupère chaque mot, ne garde que les mots-clés
+    -
+
+afficher l'historique de la conversation
+    -
+    -
     -
 
 interaction avec l'api google map
@@ -19,4 +44,5 @@ envoyer les informations récupérée à l'utilisateur
     -afficher google map
     -donner reponse media wiki
 
+envoyer du contenu html
 """
--- app/views.py
+++ app/views.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello world !"
+#app.config.from_object("constants")
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/')
+@app.route('/index/')
+def index():
+    return render_template("index.html")
+
+
+#if __name__ == "__main__":
+#    app.run()
